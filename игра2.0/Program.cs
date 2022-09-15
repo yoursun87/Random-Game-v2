49972c3419df58bdaa452c86680efd1746e1c063
79277 <79277@LAPTOP-4JH0IHNA>
2022-09-15 16:53:05 +0300

    Добавьте файлы проекта.

---
+++ игра2.0/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Random rand = new Random();
+int a = rand.Next(50);
+int count = 1;
+Console.WriteLine("Компьютер загадал число от 0 до 50 . отгадайте его,используя 3 попытки");
+Console.WriteLine("Введите первое число:");
+int b = Convert.ToInt32((Console.ReadLine()));
+
+while (count <= 3)
+{
+    if (a == b)
+    {
+        Console.WriteLine("Поздравляем вы угадали число с X попытки");
+        break;
+    }
+    else
+    {
+
+        count++;
+        if (count == 4)
+        {
+            Console.WriteLine(" Вы не угадали число . Загаданным числом было "+ a +"!");
+
+            break;
+
+
+        }
+        Console.WriteLine("Это не число" + b + ". Попытка №" + count + ":");
+
+        b = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.ReadLine();
+
+   
+
+
+
+
+    
